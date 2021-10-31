c169745353e0322656c6d12e17dad5a8d5416219
RetainedCoda <RetainedMail@protonmail.com>
2021-10-31 20:38:38 +1100

    first commit on new acc

---
+++ Current_Weight_On_Mars/weight_mars.py
@@ -0,0 +1,8 @@
+current_weight = int(input("How much do you weigh Currently? (kg please) Also no need to put kg at the end "))
+mars_weight = current_weight * 0.38
+
+print("You will weigh on mars (it will be in kg)")
+print(mars_weight)
+
+
+
